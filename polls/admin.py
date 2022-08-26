d9f6507b3be10c93325e528cc82e31fee51fe837
Maria Luiza Lopes <ramos.luiza@escolar.ifrn.edu.br>
2022-08-26 17:28:30 -0300

    Final da Parte 2

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Question
+
+#http://127.0.0.1:8000/admin/
+admin.site.register(Question)
--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-
+#http://localhost:8000/polls/
 def index(request):
     return HttpResponse("Olá, mundo. Você está no índice de enquetes.")
