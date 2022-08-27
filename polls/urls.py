2ccb64ac2b38e248402be37ca7493fa4377e2c96
Maria Luiza Lopes <ramos.luiza@escolar.ifrn.edu.br>
2022-08-27 00:48:33 -0300

    Parte 3'

--- polls/urls.py
+++ polls/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path
 from . import views
 
+
+app_name = 'polls'
 urlpatterns = [
+    # http://localhost:8000/polls/
     path('', views.index, name='index'),
+    # ex: /polls/1/
+    path('<int:question_id>/', views.detail, name='detail'),
+    # ex: /polls/1/results/
+    path('<int:question_id>/results/', views.results, name='results'),
+    # ex: /polls/1/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
